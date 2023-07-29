18caf2176e3a8236c575ea8657b2ae6a6a62452c
Erkan Kamber <erkan_kamber@abv.bg>
2023-07-29 22:22:12 +0300

    NL-22 Create User controller class and endpoints and Updated method return type

---
+++ src/main/java/erkamber/dtos/UserLoginDto.java
@@ -0,0 +1,29 @@
+package erkamber.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+public class UserLoginDto {
+
+    @NotBlank(message = "UserName cannot be Blank!")
+    @Size(min = 1, message = "UserName cannot be Empty!")
+    private String userName;
+
+    @NotBlank(message = "User Password cannot be Blank!")
+    @Size(min = 1, message = "User Password cannot be Empty!")
+    private String userPassword;
+
+    public UserLoginDto() {
+    }
+
+    public UserLoginDto(String userName, String userPassword) {
+        this.userName = userName;
+        this.userPassword = userPassword;
+    }
+}

fd187d4aa2347eaa570993f778b394ba5cb2892b
IvanKaragyozov <ivankaragyozov0@gmail.com>
2023-08-01 22:43:09 +0300

    NL-8: Created BookmarkRepository

---
+++ src/main/java/erkamber/repositories/BookmarkRepository.java
@@ -0,0 +1,12 @@
+package erkamber.repositories;
+
+import erkamber.entities.Bookmark;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookmarkRepository extends JpaRepository<Bookmark, Integer> {
+
+
+
+}

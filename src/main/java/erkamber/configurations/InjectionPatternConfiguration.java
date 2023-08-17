a33996002f584ee4faa167503a21675cfd9730ec
Erkan Kamber <erkan_kamber@abv.bg>
2023-08-17 23:27:38 +0300

    NL-34 Add BookmarkServiceImpl javadocs

--- src/main/java/erkamber/configurations/InjectionPatternConfiguration.java
+++ src/main/java/erkamber/configurations/InjectionPatternConfiguration.java
@@ -7,8 +7,6 @@ public class InjectionPatternConfiguration {
 
     String injectionPattern = ".*[;|&$`\\\'\"<>].*";
 
-
-    //Hibernate has its built-in security features, it can handle SQL injection by automatically  sanitizing input
     String sqlInjectionPattern = ".*\\b(ALTER|CREATE|DELETE|DROP|EXEC(UTE){0,1}|INSERT( +INTO){0,1}|MERGE|SELECT|UPDATE)\\b.*";
 
     public String getInjectionPattern() {

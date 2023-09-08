3e3c2ee8f977af5e381816ed176806ce0d3d4acc
Erkan Kamber <erkan_kamber@abv.bg>
2023-09-08 21:21:11 +0300

    NL-0 Add logging in email sending functionality

--- src/main/java/erkamber/services/implementations/EmailServiceImpl.java
+++ src/main/java/erkamber/services/implementations/EmailServiceImpl.java
@@ -1,6 +1,9 @@
 package erkamber.services.implementations;
 
+import erkamber.handlers.GlobalExceptionHandler;
 import erkamber.services.interfaces.EmailService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,8 @@ public class EmailServiceImpl implements EmailService {
     private final JavaMailSender javaMailSender;
 
     private final ITemplateEngine templateEngine;
+
+    private final Logger logger = LogManager.getLogger(EmailServiceImpl.class);
 
     public EmailServiceImpl(JavaMailSender javaMailSender, ITemplateEngine templateEngine) {
         this.javaMailSender = javaMailSender;
@@ -36,23 +41,29 @@ public class EmailServiceImpl implements EmailService {
     public void sendEmail(String newsAuthorName, String commentAuthorName, String comment, String authorEmail, String postTitle)
             throws MessagingException {
 
-        // Prepare the email content using a template engine
-        Context context = new Context();
-        context.setVariable("newsAuthorName", newsAuthorName);
-        context.setVariable("commentAuthorName", commentAuthorName);
-        context.setVariable("comment", comment);
-        context.setVariable("postTitle", postTitle);
+        try {
+            // Prepare the email content using a template engine
+            Context context = new Context();
+            context.setVariable("newsAuthorName", newsAuthorName);
+            context.setVariable("commentAuthorName", commentAuthorName);
+            context.setVariable("comment", comment);
+            context.setVariable("postTitle", postTitle);
 
-        String process = templateEngine.process("/email", context);
+            String process = templateEngine.process("/email", context);
 
-        // Create and send the email message
-        MimeMessage message = javaMailSender.createMimeMessage();
-        MimeMessageHelper helper;
-        helper = new MimeMessageHelper(message, true);
-        helper.setTo(authorEmail);
-        helper.setSubject("New Comment Left");
-        helper.setText(process, true);
+            // Create and send the email message
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper;
+            helper = new MimeMessageHelper(message, true);
+            helper.setTo(authorEmail);
+            helper.setSubject("New Comment Left");
+            helper.setText(process, true);
 
-        javaMailSender.send(message);
+            javaMailSender.send(message);
+        }
+        catch (Exception exception) {
+
+            logger.error("EMAIL ERROR " + exception.getMessage());
+        }
     }
 }

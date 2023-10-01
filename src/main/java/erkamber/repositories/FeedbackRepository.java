ce561d1ad67164e223a8beba17318c88caeeb408
Erkan Kamber <erkan_kamber@abv.bg>
2023-10-01 18:47:46 +0300

    NL-54 Create Feedback entity mapper

--- src/main/java/erkamber/repositories/FeedbackRepository.java
+++ src/main/java/erkamber/repositories/FeedbackRepository.java
@@ -3,11 +3,13 @@ package erkamber.repositories;
 import erkamber.entities.Feedback;
 import erkamber.entities.Media;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Integer> {
 
     List<Feedback> findFeedbackByAuthorID(int feedbackAuthorID);

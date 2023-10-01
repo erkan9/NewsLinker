66bfc8e38d10b9e577d55f2479d8b9a9e8ec91c4
Erkan Kamber <erkan_kamber@abv.bg>
2023-10-01 18:29:07 +0300

    NL-54 Create FeedbackDto entity

---
+++ src/main/java/erkamber/dtos/FeedbackDto.java
@@ -0,0 +1,27 @@
+package erkamber.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FeedbackDto {
+
+    @PositiveOrZero
+    int feedbackID;
+
+    UserDto authorDto;
+
+    @NotBlank(message = "Feedback Content cannot be Blank")
+    @NotEmpty(message = "Feedback Content cannot be Empty")
+    @NotNull(message = "Feedback Content cannot be Null")
+    @Size(min = 2, max = 300, message = "Feedback Content length could be max 500 characters")
+    private String feedbackContent;
+
+}
